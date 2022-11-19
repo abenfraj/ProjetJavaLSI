8702869f15f6250bfa508799365076da359b9a30
mog4dor <adamkar94@gmail.com>
2022-11-19 13:11:21 +0100

    Implemetation of first transformation and refactor

---
+++ src/Transformation.java
@@ -0,0 +1,9 @@
+public interface Transformation {
+
+    public void translation(int value);
+    public void homothetie(int value);
+    public void rotation();
+    public void symetrieX();
+    public void symetrieY();
+
+}
